f60989896f1baf898236a011f5df4d3601365f99
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-03-01 00:41:40 +0300

    bug fixed

--- app/src/main/java/com/asimodabas/art_book/view/ArtDetailsFragment.kt
+++ app/src/main/java/com/asimodabas/art_book/view/ArtDetailsFragment.kt
@@ -2,12 +2,37 @@ package com.asimodabas.art_book.view
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.asimodabas.art_book.R
+import com.asimodabas.art_book.databinding.FragmentArtDetailBinding
 
 class ArtDetailsFragment : Fragment(R.layout.fragment_art_detail) {
 
+    private var fragmentBinding : FragmentArtDetailBinding?=null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentArtDetailBinding.bind(view)
+        fragmentBinding = binding
+
+        binding.artImageView.setOnClickListener {
+            findNavController().navigate(ArtDetailsFragmentDirections.actionArtDetailsFragmentToImageAPIFragment())
+        }
+
+        val callback = object : OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(callback)
+    }
+
+    override fun onDestroyView() {
+        fragmentBinding = null
+        super.onDestroyView()
+
     }
 }
