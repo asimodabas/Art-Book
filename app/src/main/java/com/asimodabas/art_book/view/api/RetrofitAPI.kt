e7491a2b4a673089763ddcb28b753f9f0d7abd84
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-03-02 18:35:00 +0300

    retrofit

---
+++ app/src/main/java/com/asimodabas/art_book/view/api/RetrofitAPI.kt
@@ -0,0 +1,16 @@
+package com.asimodabas.art_book.view.api
+
+import com.asimodabas.art_book.view.model.ImageResponse
+import com.asimodabas.art_book.view.util.Util.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RetrofitAPI {
+
+    @GET("/api/")
+    suspend fun imageSearch(
+        @Query("q") searchQuery : String,
+        @Query("key") apiKey : String = API_KEY
+    ) : Response<ImageResponse>
+}
