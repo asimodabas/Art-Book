6a26beae0bdebb0af6bcc6699e169cacec367af8
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-03-03 02:06:11 +0300

    repository+

--- app/src/main/java/com/asimodabas/art_book/view/repo/ArtRepository.kt
+++ app/src/main/java/com/asimodabas/art_book/view/repo/ArtRepository.kt
@@ -1,4 +1,44 @@
 package com.asimodabas.art_book.view.repo
 
-class ArtRepository {
+import androidx.lifecycle.LiveData
+import com.asimodabas.art_book.view.api.RetrofitAPI
+import com.asimodabas.art_book.view.model.ImageResponse
+import com.asimodabas.art_book.view.roomdb.Art
+import com.asimodabas.art_book.view.roomdb.ArtDao
+import com.asimodabas.art_book.view.util.Resource
+import java.lang.Exception
+import javax.inject.Inject
+
+class ArtRepository @Inject constructor(
+    private val artDao: ArtDao,
+    private val retrofitAPI: RetrofitAPI) : ArtRepositoryInterface
+{
+
+    override suspend fun insertArt(art: Art) {
+        artDao.insertArt(art)
+    }
+
+    override suspend fun deleteArt(art: Art) {
+        artDao.deleteArt(art)
+    }
+
+    override fun getArt(): LiveData<List<Art>> {
+        return artDao.observeArts()
+    }
+
+    override suspend fun searchImage(imageString: String): Resource<ImageResponse> {
+        return try {
+
+            val response = retrofitAPI.imageSearch(imageString)
+            if (response.isSuccessful){
+                response.body()?.let {
+                    return@let Resource.success(it)
+                } ?:Resource.error("Error",null)
+            }else{
+                Resource.error("Error",null)
+            }
+        }catch (e:Exception){
+            Resource.error("No Data",null)
+        }
+    }
 }
