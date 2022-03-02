939e92931dca21f0b9e951f4fcf878983c96eaaa
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-03-03 01:22:10 +0300

    hilt

---
+++ app/src/main/java/com/asimodabas/art_book/view/ArtBookApplication.kt
@@ -0,0 +1,8 @@
+package com.asimodabas.art_book.view
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ArtBookApplication : Application() {
+}
---
+++ app/src/main/java/com/asimodabas/art_book/view/dependencyinjection/AppModule.kt
@@ -0,0 +1,44 @@
+package com.asimodabas.art_book.view.dependencyinjection
+
+import android.content.Context
+import androidx.room.Room
+import com.asimodabas.art_book.view.api.RetrofitAPI
+import com.asimodabas.art_book.view.roomdb.ArtDatabase
+import com.asimodabas.art_book.view.util.Util.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun injectRoomDatabase(@ApplicationContext context: Context) =
+        Room.databaseBuilder(
+            context,
+            ArtDatabase::class.java,
+            "ArtBookDB"
+        ).build()
+
+    @Singleton
+    @Provides
+    fun injectDao(database: ArtDatabase) = database.artDao()
+
+    @Singleton
+    @Provides
+    fun injectRetrofitAPI(): RetrofitAPI {
+        return Retrofit.Builder().addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(RetrofitAPI::class.java)
+    }
+
+}
