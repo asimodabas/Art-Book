950de9b7006df58804af2d1c66c552ffae59f2da
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-03-04 17:52:23 +0300

    MutableLiveDatas is defined

--- app/src/main/java/com/asimodabas/art_book/view/viewmodel/ArtViewModel.kt
+++ app/src/main/java/com/asimodabas/art_book/view/viewmodel/ArtViewModel.kt
@@ -1,14 +1,37 @@
 package com.asimodabas.art_book.view.viewmodel
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.asimodabas.art_book.view.model.ImageResponse
 import com.asimodabas.art_book.view.repo.ArtRepositoryInterface
+import com.asimodabas.art_book.view.roomdb.Art
+import com.asimodabas.art_book.view.util.Resource
 import javax.inject.Inject
 
 class ArtViewModel @ViewModelInject constructor(
 private val repository : ArtRepositoryInterface
 ) :ViewModel() {
+
+    //For Art Fragment
     val artList = repository.getArt()
 
+    //For ImageApi Fragment
+    private val images = MutableLiveData<Resource<ImageResponse>>()
+    val imageList : LiveData<Resource<ImageResponse>>
+    get() = images
+
+    private val selectedImage = MutableLiveData<String>()
+    val selectedImageURL : LiveData<String>
+    get() = selectedImage
+
+    private var insertArtMsg = MutableLiveData<Resource<Art>>()
+    val insertArtMessage : LiveData<Resource<Art>>
+    get() = insertArtMsg
+
+    fun resetInsertArtMessage (){
+        insertArtMsg = MutableLiveData<Resource<Art>>()
+    }
 
 }
