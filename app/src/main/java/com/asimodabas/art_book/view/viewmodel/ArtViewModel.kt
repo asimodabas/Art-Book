cc2a308d640cf73e7b39d6717f84908569de0e19
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-03-04 18:08:47 +0300

    Resource and artMessages added

--- app/src/main/java/com/asimodabas/art_book/view/viewmodel/ArtViewModel.kt
+++ app/src/main/java/com/asimodabas/art_book/view/viewmodel/ArtViewModel.kt
@@ -4,10 +4,13 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.asimodabas.art_book.view.model.ImageResponse
 import com.asimodabas.art_book.view.repo.ArtRepositoryInterface
 import com.asimodabas.art_book.view.roomdb.Art
 import com.asimodabas.art_book.view.util.Resource
+import kotlinx.coroutines.launch
+import java.lang.Exception
 import javax.inject.Inject
 
 class ArtViewModel @ViewModelInject constructor(
@@ -32,6 +35,46 @@ private val repository : ArtRepositoryInterface
 
     fun resetInsertArtMessage (){
         insertArtMsg = MutableLiveData<Resource<Art>>()
+    }
+
+    fun setSelectedImage(url : String){
+        selectedImage.postValue(url)
+    }
+
+    fun deleteARt(art:Art) = viewModelScope.launch{
+        repository.deleteArt(art)
+    }
+
+    fun insertArt(art: Art) = viewModelScope.launch{
+        repository.insertArt(art)
+    }
+
+    fun makeArt(name:String,artistName:String,year:String){
+        if (name.isEmpty() || artistName.isEmpty() || year.isEmpty()){
+            insertArtMsg.postValue(Resource.error("Please fill in the blanks",null))
+            return
+        }
+        val yearInt = try {
+            year.toInt()
+        }catch (e:Exception){
+            insertArtMsg.postValue(Resource.error("Please enter the number",null))
+            return
+        }
+        val art = Art(name,artistName,yearInt,selectedImage.value?:"")
+        insertArt(art)
+        setSelectedImage("")
+        insertArtMsg.postValue(Resource.success(art))
+    }
+
+    fun searchImage(searchString: String){
+        if (searchString.isEmpty()){
+            return
+        }
+        images.value = Resource.loading(null)
+        viewModelScope.launch {
+            val response = repository.searchImage(searchString)
+            images.value=response
+        }
     }
 
 }
