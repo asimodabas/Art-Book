97e8a6ede2b33abe801b08c0e05ec75cf3d6e0be
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-03-08 21:40:21 +0300

    Transactions related to art fragment

--- app/src/main/java/com/asimodabas/art_book/view/view/ArtFragment.kt
+++ app/src/main/java/com/asimodabas/art_book/view/view/ArtFragment.kt
@@ -3,9 +3,13 @@ package com.asimodabas.art_book.view.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.asimodabas.art_book.R
 import com.asimodabas.art_book.databinding.FragmentArtsBinding
 import com.asimodabas.art_book.view.adapter.ArtRecyclerAdapter
@@ -19,6 +23,23 @@ class ArtFragment @Inject constructor(
     private var fragmentBinding: FragmentArtsBinding? = null
     lateinit var viewModel :ArtViewModel
 
+    private val swipeCallBack = object :ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT){
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            return true
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val layoutPosition = viewHolder.layoutPosition
+            val selectedArt = artRecyclerAdapter.arts[layoutPosition]
+            viewModel.deleteARt(selectedArt)
+        }
+
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -27,13 +48,20 @@ class ArtFragment @Inject constructor(
         val binding = FragmentArtsBinding.bind(view)
         fragmentBinding = binding
 
+        subscribeToObservers()
+        binding.recyclerViewArt.adapter = artRecyclerAdapter
+        binding.recyclerViewArt.layoutManager = LinearLayoutManager(requireContext())
+        ItemTouchHelper(swipeCallBack).attachToRecyclerView(binding.recyclerViewArt)
+
         binding.fab.setOnClickListener {
             findNavController().navigate(ArtFragmentDirections.actionArtFragmentToArtDetailsFragment())
         }
     }
 
     private fun subscribeToObservers(){
-
+        viewModel.artList.observe(viewLifecycleOwner, Observer {
+            artRecyclerAdapter.arts = it
+        })
     }
 
     override fun onDestroy() {
