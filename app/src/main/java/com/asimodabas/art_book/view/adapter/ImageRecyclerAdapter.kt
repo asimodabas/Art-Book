2f6006faed6fa4eb8ef0a448af976264339d43e0
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-03-10 22:01:28 +0300

    bug fixed

--- app/src/main/java/com/asimodabas/art_book/view/adapter/ImageRecyclerAdapter.kt
+++ app/src/main/java/com/asimodabas/art_book/view/adapter/ImageRecyclerAdapter.kt
@@ -13,14 +13,14 @@ import com.bumptech.glide.RequestManager
 import javax.inject.Inject
 
 class ImageRecyclerAdapter @Inject constructor(
-    val glide :RequestManager
-):RecyclerView.Adapter<ImageRecyclerAdapter.ImageViewHolder>() {
+    val glide: RequestManager
+) : RecyclerView.Adapter<ImageRecyclerAdapter.ImageViewHolder>() {
 
     class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
-    private var onItemClickListener : ((String)->Unit)?=null
+    private var onItemClickListener: ((String) -> Unit)? = null
 
-    private val diffUtil = object : DiffUtil.ItemCallback<String>(){
+    private val diffUtil = object : DiffUtil.ItemCallback<String>() {
         override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
             return oldItem == newItem
         }
@@ -30,19 +30,19 @@ class ImageRecyclerAdapter @Inject constructor(
         }
     }
 
-    private val recyclerDiffer = AsyncListDiffer(this,diffUtil)
+    private val recyclerDiffer = AsyncListDiffer(this, diffUtil)
 
-    var images :List<String>
+    var images: List<String>
         get() = recyclerDiffer.currentList
         set(value) = recyclerDiffer.submitList(value)
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.image_row,parent,false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.image_row, parent, false)
         return ImageViewHolder(view)
     }
 
-    fun setonItemClickListener(listener:(String)->Unit){
+    fun setonItemClickListener(listener: (String) -> Unit) {
         onItemClickListener = listener
     }
 
@@ -51,7 +51,7 @@ class ImageRecyclerAdapter @Inject constructor(
         val url = images[position]
         holder.itemView.apply {
             glide.load(url).into(imageView)
-            setonItemClickListener {
+            setOnClickListener {
                 onItemClickListener?.let {
                     it(url)
                 }
