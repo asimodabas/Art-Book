ebc06050f4d017872248fb40e22fd61ff1cf147b
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-03-10 21:43:43 +0300

    subscription Observers  added

--- app/src/main/java/com/asimodabas/art_book/view/view/ImageAPIFragment.kt
+++ app/src/main/java/com/asimodabas/art_book/view/view/ImageAPIFragment.kt
@@ -2,9 +2,17 @@ package com.asimodabas.art_book.view.view
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.asimodabas.art_book.R
+import com.asimodabas.art_book.databinding.FragmentImageApiBinding
 import com.asimodabas.art_book.view.adapter.ImageRecyclerAdapter
+import com.asimodabas.art_book.view.util.Status
+import com.asimodabas.art_book.view.viewmodel.ArtViewModel
 import com.bumptech.glide.RequestManager
 import javax.inject.Inject
 
@@ -12,8 +20,48 @@ class ImageAPIFragment @Inject constructor(
     private val imageRecyclerAdapter: ImageRecyclerAdapter
 ) : Fragment(R.layout.fragment_image_api) {
 
+    lateinit var viewModel : ArtViewModel
+    private var fragmentBinding : FragmentImageApiBinding?=null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(requireActivity()).get(ArtViewModel::class.java)
+        val binding = FragmentImageApiBinding.bind(view)
+        fragmentBinding = binding
+
+        subscribeToObservers()
+
+        binding.imageRecyclerView.adapter = imageRecyclerAdapter
+        binding.imageRecyclerView.layoutManager = GridLayoutManager(requireContext(),3)
+        imageRecyclerAdapter.setonItemClickListener {
+            findNavController().popBackStack()
+            viewModel.setSelectedImage(it)
+        }
+    }
+
+    fun subscribeToObservers(){
+        viewModel.imageList.observe(viewLifecycleOwner, Observer {
+            when(it.status){
+
+                Status.SUCCESS -> {
+                    val urls = it.data?.hits?.map {
+                        imageResult ->
+                        imageResult.previewURL
+                    }
+                    imageRecyclerAdapter.images=urls?: listOf()
+                    fragmentBinding?.progesBar?.visibility=View.GONE
+                }
+                Status.ERROR -> {
+                    Toast.makeText(requireContext(),it.message?:"Error",Toast.LENGTH_LONG).show()
+                    fragmentBinding?.progesBar?.visibility=View.GONE
+
+                }
+                Status.LOADING ->{
+                    fragmentBinding?.progesBar?.visibility=View.VISIBLE
+                }
+            }
+        })
     }
 
 }
