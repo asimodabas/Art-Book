c13a643c932123a30bd886e6b8986a18ee8f2e8d
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-03-21 03:25:46 +0300

    gradle update and insertArt testing

--- app/src/androidTest/java/com/asimodabas/art_book/roomdb/ArtDaoTest.kt
+++ app/src/androidTest/java/com/asimodabas/art_book/roomdb/ArtDaoTest.kt
@@ -5,10 +5,17 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import androidx.test.filters.SmallTest
+import com.asimodabas.art_book.getOrAwaitValue
+import com.asimodabas.art_book.view.roomdb.Art
 import com.asimodabas.art_book.view.roomdb.ArtDao
 import com.asimodabas.art_book.view.roomdb.ArtDatabase
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 @SmallTest
 @ExperimentalCoroutinesApi
@@ -28,6 +35,28 @@ class ArtDaoTest {
 
         dao = database.artDao()
     }
+
+    @After
+    fun teardown(){
+        database.close()
+    }
+
+    @Test
+    fun insertArtTesting() = runBlockingTest{
+
+        val exampleArt = Art("Mona Lisa","Da Vinci",1700,"test.com",1)
+        dao.insertArt(exampleArt)
+
+        val list = dao.observeArts().getOrAwaitValue()
+        assertThat(list).contains(exampleArt)
+
+    }
+
+    @Test
+    fun deleteArtTesting()= runBlockingTest{
+
+    }
+
 
 
 }
