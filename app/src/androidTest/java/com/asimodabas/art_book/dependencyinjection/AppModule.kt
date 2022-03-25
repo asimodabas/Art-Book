6fdb4f1882878f7c6dd2075f8aa70ddd8a4a08a6
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-03-25 04:23:40 +0300

    art fragment testing

--- app/src/androidTest/java/com/asimodabas/art_book/dependencyinjection/AppModule.kt
+++ app/src/androidTest/java/com/asimodabas/art_book/dependencyinjection/AppModule.kt
@@ -1,7 +1,6 @@
-package com.asimodabas.art_book.view.dependencyinjection
+package com.asimodabas.art_book.dependencyinjection
 
 import android.content.Context
-import androidx.room.Dao
 import androidx.room.Room
 import com.asimodabas.art_book.R
 import com.asimodabas.art_book.view.api.RetrofitAPI
--- app/src/androidTest/java/com/asimodabas/art_book/view/ArtFragmentTest.kt
+++ app/src/androidTest/java/com/asimodabas/art_book/view/ArtFragmentTest.kt
@@ -1,11 +1,23 @@
 package com.asimodabas.art_book.view
 
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.filters.MediumTest
+import com.asimodabas.art_book.R
+import com.asimodabas.art_book.launchFragmentInHiltContainer
+import com.asimodabas.art_book.view.view.ArtFragment
+import com.asimodabas.art_book.view.view.ArtFragmentDirections
+import com.asimodabas.art_book.view.view.ArtFragmentFactory
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.Mockito
+import javax.inject.Inject
 
 @MediumTest
 @HiltAndroidTest
@@ -14,6 +26,9 @@ class ArtFragmentTest {
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
+    @Inject
+    lateinit var fragmentFactory : ArtFragmentFactory
+
     @Before
     fun setup (){
         hiltRule.inject()
@@ -21,7 +36,18 @@ class ArtFragmentTest {
 
     @Test
     fun testNavigationFromArtToArtDetails(){
-        
+
+        val navController = Mockito.mock(NavController::class.java)
+
+        launchFragmentInHiltContainer<ArtFragment>(factory = fragmentFactory) {
+            Navigation.setViewNavController(requireView(),navController)
+        }
+
+        Espresso.onView(ViewMatchers.withId(R.id.fab)).perform(click())
+        Mockito.verify(navController).navigate(
+            ArtFragmentDirections.actionArtFragmentToArtDetailsFragment()
+        )
+
     }
 
 
