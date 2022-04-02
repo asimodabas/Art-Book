a9726af4e92ac8430426705451e8e4c7e1059dd6
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-04-02 04:27:53 +0300

    Navigation From Details ImageAPI test

--- app/src/androidTest/java/com/asimodabas/art_book/view/ArtDetailsFragmentTest.kt
+++ app/src/androidTest/java/com/asimodabas/art_book/view/ArtDetailsFragmentTest.kt
@@ -1,13 +1,24 @@
 package com.asimodabas.art_book.view
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.filters.MediumTest
+import com.asimodabas.art_book.R
+import com.asimodabas.art_book.launchFragmentInHiltContainer
+import com.asimodabas.art_book.view.view.ArtDetailsFragment
+import com.asimodabas.art_book.view.view.ArtDetailsFragmentDirections
 import com.asimodabas.art_book.view.view.ArtFragmentFactory
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito
 import javax.inject.Inject
 
 @MediumTest
@@ -29,7 +40,21 @@ class ArtDetailsFragmentTest {
         hiltRule.inject()
     }
 
+    @Test
+    fun testNavigationFromDetailsToImageAPI() {
 
+        val navController = Mockito.mock(NavController::class.java)
 
+        launchFragmentInHiltContainer<ArtDetailsFragment>(
+            factory = fragmentFactory
+        ){
+            Navigation.setViewNavController(requireView(),navController)
+        }
 
+        Espresso.onView(ViewMatchers.withId(R.id.artImageView)).perform(click())
+
+        Mockito.verify(navController).navigate(
+            ArtDetailsFragmentDirections.actionArtDetailsFragmentToImageAPIFragment()
+        )
+    }
 }
