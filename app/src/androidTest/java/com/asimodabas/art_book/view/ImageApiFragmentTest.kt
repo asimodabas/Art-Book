e36c484ec78fe039e5a4ce5f2a026562ddef4472
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-04-12 06:39:45 +0300

    Image api fragment tests

---
+++ app/src/androidTest/java/com/asimodabas/art_book/view/ImageApiFragmentTest.kt
@@ -0,0 +1,66 @@
+package com.asimodabas.art_book.view
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.filters.MediumTest
+import com.asimodabas.art_book.R
+import com.asimodabas.art_book.launchFragmentInHiltContainer
+import com.asimodabas.art_book.repo.FakeArtRepositoryTest
+import com.asimodabas.art_book.view.adapter.ImageRecyclerAdapter
+import com.asimodabas.art_book.view.view.ArtFragmentFactory
+import com.asimodabas.art_book.view.view.ImageAPIFragment
+import com.asimodabas.art_book.view.viewmodel.ArtViewModel
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito
+import javax.inject.Inject
+
+@MediumTest
+@HiltAndroidTest
+@ExperimentalCoroutinesApi
+class ImageApiFragmentTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var fragmentFactory: ArtFragmentFactory
+
+    @Before
+    fun setup (){
+        hiltRule.inject()
+    }
+
+    @Test
+    fun selectImage(){
+        val navController = Mockito.mock(NavController::class.java)
+
+        val testViewModel = ArtViewModel(FakeArtRepositoryTest())
+
+        launchFragmentInHiltContainer<ImageAPIFragment>(
+            factory = fragmentFactory
+        ){
+            Navigation.setViewNavController(requireView(),navController)
+            viewModel =testViewModel
+        }
+
+        Espresso.onView(withId(R.id.imageRecyclerView)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<ImageRecyclerAdapter.ImageViewHolder>(
+                0,click()
+            )
+        )
+    }
+
+}
